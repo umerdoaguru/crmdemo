cce0f7c397790a588ac8129a8c32ce206e2e5993
umerqureshifine <umerqureshidoaguru@gmail.com>
2025-04-24 13:06:16 +0530

    stmp

--- server/controllers/smtp.js
+++ server/controllers/smtp.js
@@ -39,3 +39,81 @@
   //     });
   //   });
   // };
+  const sendOtpEmployee = (req, res) => {
+    const { email } = req.body;
+  
+    const selectQuery = "SELECT * FROM employee WHERE email = ?";
+  
+    db.query(selectQuery, email, (err, result) => {
+      if (err) {
+        return res.status(400).json({ success: false, message: err.message });
+      } else {
+        if (!result || result.length === 0) {
+          return res.status(404).json({ success: false, message: "Email not found" });
+          
+        } 
+        else {
+          
+          
+          
+          // Random OTP generation
+          function generateOTP(length) {
+            const chars = "0123456789";
+            let otp = "";
+  
+            for (let i = 0; i < length; i++) {
+              const randomIndex = Math.floor(Math.random() * chars.length);
+              otp += chars[randomIndex];
+            }
+  
+            return otp;
+          }
+  
+          const OTP = generateOTP(6);
+  
+          try {
+              
+          const transporter = nodemailer.createTransport({
+           host: 'mail.one-realty.in',
+           port: 465,
+           secure: true, // Use SSL
+           auth: {
+                user: 'info@one-realty.in',
+                 pass: 'onerealty@123',
+           },
+         });
+            
+  
+            const mailOptions = {
+              from: 'info@one-realty.in',
+              to: email,
+              subject: "Employee Password Reset OTP",
+              text: `Your OTP for password reset is: ${OTP}`,
+            };
+  
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                console.error(error);
+                return res.status(500).json("An error occurred while sending the email.");
+              } else {
+                console.log("OTP sent:", info.response);
+  
+                const updateQuery = "INSERT INTO otpcollections (email, code) VALUES (?, ?) ON DUPLICATE KEY UPDATE code = VALUES(code)";
+                db.query(updateQuery, [email, OTP], (upErr, upResult) => {
+                  if (upErr) {
+                    return res.status(400).json({ success: false, message: upErr.message });
+                  }
+                  return res.status(200).json({ message: "OTP sent successfully" });
+                });
+              }
+            });
+          } catch (error) {
+            console.log(error);
+            return res.status(500).json("An error occurred.");
+          }
+       
+      }
+      }
+    });
+  };
+  
